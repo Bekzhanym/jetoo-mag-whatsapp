5c4b6d1d5ee8c712bbc859775058f3e2f4b94f16
Bekzhanym <maukenovbekzan@gmail.com>
2025-07-31 16:09:05 +0500

    second

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import mobImage from '../assets/mob.jpg';
 import './WelcomeScreen.css';
 
@@ -7,14 +7,30 @@ interface WelcomeScreenProps {
 }
 
 const WelcomeScreen: React.FC<WelcomeScreenProps> = ({ onStart }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    // Предзагружаем изображение
+    const img = new Image();
+    img.onload = () => setImageLoaded(true);
+    img.src = mobImage;
+  }, []);
+
   return (
     <div className="welcome-screen">
       <div className="welcome-container">
+        {!imageLoaded && (
+          <div className="loading-overlay">
+            <div className="loading-spinner"></div>
+            <p>Жүктелуде...</p>
+          </div>
+        )}
         <img 
           src={mobImage} 
           alt="JETOO ENGLISH Welcome" 
-          className="welcome-image"
+          className={`welcome-image ${imageLoaded ? 'loaded' : ''}`}
           onClick={onStart}
+          onLoad={() => setImageLoaded(true)}
         />
       </div>
     </div>
